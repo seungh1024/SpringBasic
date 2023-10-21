2a97bd28b24ff1b15d23c717431795520b0a5ab2
seungh <seungh1024@naver.com>
2023-10-21 23:00:08 +0900

    순수 자바 코드 -> 스프링으로 전환
    ApplicationContext를 사용해서 스프링 빈을 찾아서 사용하도록 변경

--- server/core/src/main/java/com/seungh1024/AppConfig.java
+++ server/core/src/main/java/com/seungh1024/AppConfig.java
@@ -1,5 +1,6 @@
 package com.seungh1024;
 
+import com.seungh1024.discount.DiscountPolicy;
 import com.seungh1024.discount.FixDiscountPolicy;
 import com.seungh1024.discount.RateDiscountPolicy;
 import com.seungh1024.member.MemberService;
@@ -7,14 +8,30 @@ import com.seungh1024.member.MemberServiceImpl;
 import com.seungh1024.member.MemoryMemberRepository;
 import com.seungh1024.order.OrderService;
 import com.seungh1024.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- server/core/src/main/java/com/seungh1024/OrderApp.java
+++ server/core/src/main/java/com/seungh1024/OrderApp.java
@@ -7,12 +7,17 @@ import com.seungh1024.member.MemberServiceImpl;
 import com.seungh1024.order.Order;
 import com.seungh1024.order.OrderService;
 import com.seungh1024.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
